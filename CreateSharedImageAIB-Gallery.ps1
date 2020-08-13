5ae848c72006407503a4de13814fbd630292dc5a
virtualosityinc <devante@virtualosity-inc.com>
2020-08-13 11:44:53 -0500

    Update CreateSharedImageAIB-Gallery.ps1

--- CreateSharedImageAIB-Gallery.ps1
+++ CreateSharedImageAIB-Gallery.ps1
@@ -29,6 +29,11 @@ $BNPimageDefName ="win10entpersonalimage"
 # additional replication region
 # $BNPreplRegion2="eastus"
 
+# setup role def names, these need to be unique
+$BNPtimeInt=$(get-date -UFormat "%s")
+$BNPimageRoleDefName="Azure Image Builder Image Def"+$BNPtimeInt
+$BNPidentityName="aibIdentity"+$BNPtimeInt
+
 # Create the gallery
 New-AzGallery `
    -GalleryName $BNPsigGalleryName `
@@ -46,3 +51,31 @@ New-AzGalleryImageDefinition `
    -Publisher 'BNPRCCTEST' `
    -Offer 'Windows10Enterprise' `
    -Sku '19h2-ent'
+
+
+##############################################################
+# Download and configure the template
+##############################################################
+
+$BNPtemplateFilePath = "armTemplateWinSIG.json"
+
+Invoke-WebRequest `
+   -Uri "https://github.com/virtualosityinc/DevanteAzureAIB/blob/master/armTemplateWinSIG.json" `
+   -OutFile $BNPtemplateFilePath `
+   -UseBasicParsing
+
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<subscriptionID>',$BNPsubscriptionID | Set-Content -Path $BNPtemplateFilePath
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<rgName>',$BNPimageResourceGroup | Set-Content -Path $BNPtemplateFilePath
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<runOutputName>',$BNPrunOutputName | Set-Content -Path $BNPtemplateFilePath
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<imageDefName>',$BNPimageDefName | Set-Content -Path $BNPtemplateFilePath
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<sharedImageGalName>',$BNPsigGalleryName | Set-Content -Path $BNPtemplateFilePath
+(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<region1>',$BNPlocation | Set-Content -Path $BNPtemplateFilePath
+<#(Get-Content -path $BNPtemplateFilePath -Raw ) `
+   -replace '<region2>',$BNPreplRegion2 | Set-Content -Path $BNPtemplateFilePath #>
+((Get-Content -path $BNPtemplateFilePath -Raw) -replace '<imgBuilderId>',$BNPidentityNameResourceId) | Set-Content -Path $BNPtemplateFilePath
