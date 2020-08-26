f4a58da3dff6c7d16e7af6f17716fa1a8334bb02
virtualosityinc <devante@virtualosity-inc.com>
2020-08-26 13:19:59 -0500

    Create DevanteAIB.ps1

---
+++ DevanteAIB.ps1
@@ -0,0 +1,91 @@
+﻿# Step 1: Import module
+Import-Module Az.Accounts
+
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
+
+# destination image resource group
+$imageResourceGroup="aibWinImg01"
+
+# location (see possible locations in main docs)
+$location="eastus"
+
+## if you need to change your subscription: Get-AzSubscription / Select-AzSubscription -SubscriptionName 
+
+# get subscription, this will get your current subscription
+$subscriptionID=$currentAzContext.Subscription.Id
+
+# name of the image to be created
+$imageName="win10image1"
+
+# image distribution metadata reference name
+$runOutputName="win10ManImg01ro"
+
+# image template name
+$imageTemplateName="window10Template01"
+
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName="win10SigR01"
+
+# create resource group for image and image template resource
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+#######################################################################################
+# Step 2 : Permissions, create user identity and role for AIB
+#######################################################################################
+# setup role def names, these need to be unique
+$timeInt=$(get-date -UFormat "%s")
+$imageRoleDefName="Azure Image Builder Image Def"+$timeInt
+$idenityName="aibIdentity"+$timeInt
+
+## Add AZ PS module to support AzUserAssignedIdentity
+Install-Module -Name Az.ManagedServiceIdentity
+
+# create identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName
+
+$idenityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+$idenityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+
+# Assign permissions for identity to distribute images
+$aibRoleImageCreationUrl="https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+# download config
+Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+# create role definition
+New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+
+# grant role definition to image builder service principal
+New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+
+### NOTE: If you see this error: 'New-AzRoleDefinition: Role definition limit exceeded. No more role definitions can be created.' See this article to resolve:
+# https://docs.microsoft.com/en-us/azure/role-based-access-control/troubleshooting
+
+# Download and update template
+# update AIB image config template
+$templateUrl="https://raw.githubusercontent.com/virtualosityinc/DevanteAzureAIB/master/helloImageTemplateWin01.json"
+$templateFilePath = "helloImageTemplateWin01.json"
+
+# download configs
+Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imageName>',$imageName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+# Submit the template
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2019-05-01-preview" -imageTemplateName $imageTemplateName -svclocation $location
+
+# note this will take minute, as validation is run (security / dependencies etc.)
+
+#Build the image
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2019-05-01-preview" -Action Run -Force
